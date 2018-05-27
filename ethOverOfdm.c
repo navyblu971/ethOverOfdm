5dd9cf258851359d87898d12d5758e01322092a0
atexide fabrice <navyblu971@hotmail.com>
2018-05-27 21:04:02 +0200

    compile ok

--- ethOverOfdm.c
+++ ethOverOfdm.c
@@ -4,7 +4,9 @@
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
 
- struct net_device *ofdm_dev ;
+struct net_device *ofdm_dev ;
+
+struct net_device_stats *stats;
 
 static void ofdm_setup(struct net_device * ) ; 
 
@@ -20,23 +22,74 @@ int ofdm_close ( struct net_device * dev)
 	netif_stop_queue(dev);
 	return 0 ; 
 }
+
+
+
+
+
+static void ofdm_rx (struct sk_buff *skb,  struct net_device * dev)
+{
+	stats->rx_packets +=1;
+	stats->rx_bytes +=skb->len; 
+	skb->protocol = eth_type_trans (skb , dev); 
+	netif_rx(skb); 
+
+
+}
+
+
+
+
+void radio_tx (char * buf, int len, struct net_device *dev)
+{
+
+printk ("radio_tx is sending data"); 
+
+} 
+
 static int ofdm_xmit (struct sk_buff *skb, 
-					struct net_device *dev)
+				 	struct net_device *dev)
 	{
 		printk ("dummy xmit function called....\n");
-		dev_kfree_skb(skb);
+		
+	int len; 
+	char * data; 
+	len = skb->len; 
+	data = skb->data; 
+	ofdm->trans_start = jiffies; 
+	stats->tx_packets +=1; 
+	stats->tx_bytes +=skb->len; 
+
+	radio_tx(data, len, ofdm_dev);
+	/* loopback ...*/
+	ofdm_rx(skb, ofdm_dev); 
+	return 0 ; 
+
+
+
+
+	dev_kfree_skb(skb);
 		return 0;
 	}
 
+
+void ofdm_get_stats (struct net_device *dev)
+{
+
+
+}
 
 static struct net_device_ops ofdm_ops = {
  /*    .ndo_init         = ofdm_init,*/
      .ndo_open         = ofdm_open,
      .ndo_stop         = ofdm_close,
      .ndo_start_xmit   = ofdm_xmit,
-/*     .ndo_do_ioctl     = ofdm_ioctl
-	.ndo_get_stats = ofdm_get_stats*/
+/*     .ndo_do_ioctl     = ofdm_ioctl*/
+	.ndo_get_stats = ofdm_get_stats
 };
+
+
+
 
 
 	
