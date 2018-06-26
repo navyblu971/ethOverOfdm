f6d16f691fc521c233482db6811ed833a9dc67cd
atexide fabrice <navyblu971@hotmail.com>
2018-06-26 17:19:10 -0400

    radio.c

---
+++ radio/radio.c
@@ -0,0 +1,25 @@
+static int K = 64 ; 
+static int CP = K//4 ; 
+static int P = 8 ; 
+static int pilotValue = 3+3j ; 
+
+static unsigned char dataCarriers[K] ; 
+static unsigned char pilotCarriers = { 0, 8, 16, 24, 32, 40, 48, 56, 63}
+
+bool initCarrier ()
+{
+	ret = false; 
+
+	int i =0
+	while (i < P)
+	{
+		dataCarriers[pilotCarriers[i]]; 
+		i++
+	}
+
+
+
+	
+
+	return ret; 
+}
