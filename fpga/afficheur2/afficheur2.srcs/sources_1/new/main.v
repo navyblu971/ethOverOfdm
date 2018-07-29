520ef6f69f39219f5e5061d6bba6231711a50974
fabrice atexide <navyblu971@hotmail.com>
2018-07-29 15:58:18 +0200

    afficheur2

---
+++ fpga/afficheur2/afficheur2.srcs/sources_1/new/main.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.07.2018 19:10:14
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+    input clk100,
+    input rst,
+    output CA,
+    output CB,
+    output CC,
+    output CD,
+    output CE,
+    output CF,
+    output CG,
+    output DP,
+    output AN
+    );
+   
+   assign CA = 1'b0 ;  
+   assign CB = 1'b1 ; 
+   assign CC = 1'b1 ; 
+   assign CD = 1'b0 ; 
+   assign CE = 1'b0 ; 
+   assign CF = 1'b0 ; 
+   assign CG = 1'b0 ; 
+   assign DP = 1'b0 ; 
+   assign AN = 1'b1 ; 
+    
+endmodule
